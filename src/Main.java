e245b6742378aa0eec85e7ba1f69e0f8418d60b5
Egurazdov <egurazdov@yandex.ru>
2024-10-13 13:48:08 +0300

    Second

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<String> list = new ArrayList<>();
+        List<String> list1 = new ArrayList<>();
+        list.add("red");
+        list.add("blue");
+        list.add("green");
+        list1.add("1");
+        list1.add("1");
+        list1.add("1");
+        Collections.copy(list1, list);
+        System.out.println(list1);
+        System.out.println("Такой цвет есть в списке - " + findElement("reeed", list));
+    }
+
+    private static boolean findElement(String colour, List<String> list) {
+        return list.contains(colour);
     }
 }
